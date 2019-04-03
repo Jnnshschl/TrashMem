f3d269b88f29ca9844921b0324fb9df30dbb97a9
Jannis <jannis@jnns.de>
2019-04-03 10:30:08 +0200

    cdecl, stdcall, fastcall asm calling examples

--- TrashMem.HookDll/dllmain.cpp
+++ TrashMem.HookDll/dllmain.cpp
@@ -26,17 +26,57 @@ extern "C" __declspec(dllexport) bool WINAPI DllMain(HINSTANCE hInstance, DWORD 
 void ExecuteOutputTrash()
 {
 	// functions from ShittyMcUlow.exe
-	unsigned int func_output_trash_cdecl = 0xD71280;
-	unsigned int func_output_trash_stdcall = 0xD712A0;
-	unsigned int func_output_trash_fastcall = 0xD712C0;
+	unsigned int func_output_trash_cdecl = 0x2412E0;
+	unsigned int func_output_trash_stdcall = 0x241300;
+	unsigned int func_output_trash_fastcall = 0x241320;
+
+	unsigned int func_output_int32_cdecl = 0x241340;
+	unsigned int func_output_int32_stdcall = 0x241360;
+	unsigned int func_output_int32_fastcall = 0x241380;
 
 	while (g_running) {
+		//# Simple function calls
+		/*
 		__asm
 		{
 			CALL func_output_trash_cdecl
 			CALL func_output_trash_stdcall
 			CALL func_output_trash_fastcall
 		};
+		*/
+
+		//# Function calls with 2 integer args
+		// CDECL, simple stuff, need to clean the stack
+		// => output_int32_cdecl(4711, 1337)
+		__asm
+		{
+			PUSH 1337
+			PUSH 4711
+			CALL func_output_int32_cdecl
+			ADD ESP, 0x8
+		};
+
+		// STDCALL, simple stuff, 
+		// no need to clean the stack
+		// => output_int32_stdcall(4711, 1337)
+		__asm
+		{
+			PUSH 1337
+			PUSH 4711
+			CALL func_output_int32_stdcall
+		};
+
+		// FASTCALL, this trash follows no standard, 
+		// it may not work with some applications
+		// => output_int32_fastcall(4711, 1337)
+		__asm
+		{
+			MOV EAX, 4711
+			MOV EBX, 1337
+			CALL func_output_int32_fastcall
+		};
+
+		// THISCALL, will follow...
 
 		Sleep(1000);
 	}

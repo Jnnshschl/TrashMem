4c3805b79a5d9e1301acd19750cbc37cf4fc321b
Jannis <jannis@jnns.de>
2019-04-03 11:06:31 +0200

    cdecl, stdcall, fastcall non inline asm calling

--- TrashMem.HookDll/dllmain.cpp
+++ TrashMem.HookDll/dllmain.cpp
@@ -1,22 +1,14 @@
 #include "dllmain.hpp"
 
-sig_atomic_t volatile g_running = 1;
-
-void sig_handler(int signum)
-{
-	if (signum == SIGINT)
-		g_running = 0;
-}
-
 extern "C" __declspec(dllexport) bool WINAPI DllMain(HINSTANCE hInstance, DWORD fwdReason, LPVOID lpvReserved)
 {
 	switch (fwdReason)
 	{
-		case DLL_PROCESS_ATTACH:
-			signal(SIGINT, sig_handler);
+	case DLL_PROCESS_ATTACH:
+		signal(SIGINT, sig_handler);
 
-			ExecuteOutputTrash();
-			break;
+		ExecuteOutputTrash();
+		break;
 
 	}
 
@@ -30,53 +22,74 @@ void ExecuteOutputTrash()
 	unsigned int func_output_trash_stdcall = 0x241300;
 	unsigned int func_output_trash_fastcall = 0x241320;
 
-	unsigned int func_output_int32_cdecl = 0x241340;
-	unsigned int func_output_int32_stdcall = 0x241360;
-	unsigned int func_output_int32_fastcall = 0x241380;
+	// function pointer
+	unsigned int p_output_int32_cdecl = 0x241340;
+	// function pointer to function typedef
+	cdecl_output_int32* func_output_int32_cdecl
+		= (cdecl_output_int32*)p_output_int32_cdecl;
+
+	unsigned int p_output_int32_stdcall = 0x241360;
+	stdcall_output_int32* func_output_int32_stdcall
+		= (stdcall_output_int32*)p_output_int32_stdcall;
+
+	unsigned int p_output_int32_fastcall = 0x241380;
+	fastcall_output_int32* func_output_int32_fastcall
+		= (fastcall_output_int32*)p_output_int32_fastcall;
 
 	while (g_running) {
 		//# Simple function calls
 		/*
 		__asm
 		{
-			CALL func_output_trash_cdecl
-			CALL func_output_trash_stdcall
-			CALL func_output_trash_fastcall
+			CALL p_output_trash_cdecl
+			CALL p_output_int32_stdcall
+			CALL p_output_int32_fastcall
 		};
 		*/
 
 		//# Function calls with 2 integer args
 		// CDECL, simple stuff, need to clean the stack
 		// => output_int32_cdecl(4711, 1337)
+		/*
 		__asm
 		{
 			PUSH 1337
 			PUSH 4711
-			CALL func_output_int32_cdecl
+			CALL p_output_int32_cdecl
 			ADD ESP, 0x8
 		};
+		*/
 
 		// STDCALL, simple stuff, 
 		// no need to clean the stack
 		// => output_int32_stdcall(4711, 1337)
+		/*
 		__asm
 		{
 			PUSH 1337
 			PUSH 4711
-			CALL func_output_int32_stdcall
+			CALL p_output_int32_stdcall
 		};
+		*/
 
 		// FASTCALL, this trash follows no standard, 
 		// it may not work with some applications
 		// => output_int32_fastcall(4711, 1337)
+		/*
 		__asm
 		{
 			MOV EAX, 4711
 			MOV EBX, 1337
-			CALL func_output_int32_fastcall
+			CALL p_output_int32_fastcall
 		};
+		*/
 
 		// THISCALL, will follow...
+
+		//# Non asm calls
+		func_output_int32_cdecl(4711, 1337);
+		func_output_int32_stdcall(4711, 1337);
+		func_output_int32_fastcall(4711, 1337);
 
 		Sleep(1000);
 	}
--- TrashMem.HookDll/dllmain.hpp
+++ TrashMem.HookDll/dllmain.hpp
@@ -6,6 +6,20 @@
 
 #include <Windows.h>
 
+sig_atomic_t volatile g_running = 1;
+
 void ExecuteOutputTrash();
+
+// ShittyMcUlow function signatures
+typedef void _cdecl cdecl_output_int32(int a, int b);
+typedef void _stdcall stdcall_output_int32(int a, int b);
+typedef void _fastcall fastcall_output_int32(int a, int b);
+
+void sig_handler(int signum)
+{
+	if (signum == SIGINT
+		&& signum == SIGTERM)
+		g_running = 0;
+}
 
 #endif
